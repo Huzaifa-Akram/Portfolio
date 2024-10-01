204a7d204c6069969c13ebc76833590e8096a484
Huzaifa-Akram <huzaifaakram121@gmail.com>
2024-10-01 13:45:26 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // Update this to match your deployment path
   plugins: [react()],
 })
+ 
